207fcae01157f5ad8a1cf18daa00e06421d5f0d0
Asmith007 <100554799+Asmith007@users.noreply.github.com>
2023-10-04 19:59:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ global.antispam = false //auto kick spammer (true to on, false to off)
 
 
 //if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
-global.keyopenai = "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"
+global.keyopenai = "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"
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['919707925737'] //ur owner number
+global.ownernomer = "919707925737" //ur owner number2
+global.ownername = "🦄Lucifer07" //ur owner name
+global.ytname = "Yt: Lucifer07" //ur yt chanel name
+global.socialm = "GitHub: Asmith007" //ur github or insta name
+global.location = "India, Assam, Silchar" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
+global.botname = "Cheems"
+global.ownernumber = '919707925737'
+global.ownername = '🦄Lucifer07'
+global.ownerNumber = ["919707925737@s.whatsapp.net"]
+global.ownerweb = "https://github.com/Asmith007"
+global.websitex = "https://github.com/Asmith007"
+global.wagc = "https://chat.whatsapp.com/Cj50afXfGDn71YWQU183ms"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
+global.wm = "Lucifer07 Bot Inc."
+global.botscript = 'https://github.com/Asmith007/CheemsBot-MD8 //script link'
+global.packname = "Lucifer07"
+global.author = "🦄Lucifer07\n\n"
+global.creator = "919707925737@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
